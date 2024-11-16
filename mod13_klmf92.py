643814551223c6bc79ddc63e6d94d975ddca4dbc
kriswizzle03 <krislmor21@gmail.com>
2024-11-15 21:19:15 -0600

    Added unit tests for checking if time_series input is 1 character and if that character is '1', '2', '3', or '4'

--- mod13_klmf92.py
+++ mod13_klmf92.py
@@ -10,21 +10,33 @@ class TestUserInput(unittest.TestCase):
         symbol = userInput.stock_symbol
         self.assertTrue(symbol.isupper(), f"Stock symbol '{symbol}' is not all capital letters.")
 
+    # symbol: alpha characters
     def test_symbol_alpha(self):
         symbol = userInput.stock_symbol
         self.assertTrue(symbol.isalpha(), f"Stock symbol {symbol} is not made up of only alpha characters")
         
-
+    # symbol: symbol length is 1 through 7 characters
     def test_symbol_length(self):
         symbol = userInput.stock_symbol
         self.assertTrue(1 <= len(symbol) <= 7, f"Stock symbol '{symbol}' is not between 1 and 7 characters.")
+
 
     # chart type: 1 numeric character, 1 or 2
     def test_chart_type_num(self):
         chart_type = userInput.chart_type
         self.assertTrue(chart_type == "1" or chart_type == "2", f"The selection {chart_type} for chart_type is not 1 or 2.")
 
-    # time series: 1 numeric character, 1 - 4
+    # time series: 1 numeric character
+    def test_time_series_length(self):
+        time_series = userInput.time_series_function
+        self.assertTrue(1 <= len(time_series) <= 4, f"Stock symbol '{time_series}' is not between 1 and 4 characters.")
+
+    # time series: options are 1-4
+    def test_time_series_length(self):
+        time_series = userInput.time_series_function
+        self.assertTrue(time_series == "1" or time_series == "2" or time_series == "3" or
+                        time_series == "4", f"Stock symbol '{time_series}' is not either '1', '2', '3', or '4'.")
+
 
     # start date: date type YYYY-MM-DD
 
